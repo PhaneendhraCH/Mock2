75ee2c8d2d32d49e847fc6e08fae6cb6b21e0881
PhaneendhraCH <phaneendhra.chinta@thundersoft.com>
2021-09-30 21:47:01 +0530

    Circular Linked List

---
+++ makefile_cll/CLL.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node_insert.h"
+#include "node_delete.h"
+#include "struct_node.h"
+
+//struct node *head,*currentnode,*newnode; // insert
+
+//struct node *odd,*temp,*append,*head; //delete
+
+void display(){
+	struct node *temp;
+	temp = head;
+	while(temp!=NULL){
+		printf("%d ",temp->nested->pid);
+	
+		if(temp->link == head)
+			break;
+		temp = temp->link;
+	}
+	printf("\n");
+}
+
+int main(){
+
+	insert(1);
+	insert(2);
+	insert(3);
+	insert(4);
+	insert(5);
+	display();
+	//delete_odd();
+	//display();
+	
+}
+
+
+
+
